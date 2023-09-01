5fb9d5879e7983348b0ea8f040e0e96e8dd29971
Jaroslav Svoboda (Krtek) <jaroslav.svoboda@elostech.cz>
2023-09-01 11:42:18 +0200

    openebs

--- init.ps1
+++ init.ps1
@@ -3,10 +3,11 @@ kubectl create namespace argocd
 helm upgrade --install --namespace  argocd  argocd argo/argo-cd -f .\infra\values\argocd.yaml
 kubectl apply -f root.yaml
 
+#kubectl port-forward service/argocd-server -n argocd 8080:443
+
+
 kubectl apply -f .\infra\auth-acmedns.yaml
 kubectl apply -f .\infra\clusterissuer.yaml
 
 $base64Pwd = kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" 
 [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($base64Pwd))
-
-kubectl port-forward service/argocd-server -n argocd 8080:443
