b9bb24f2488e43a68e75b05f26c3b75032f316fa
Jaroslav Svoboda (Krtek) <jaroslav.svoboda@elostech.cz>
2023-09-01 13:17:15 +0200

    minio tenant

--- init.ps1
+++ init.ps1
@@ -18,3 +18,6 @@ kubectl apply -f .\infra\clusterissuer.yaml
 
 $base64Pwd = kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" 
 [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($base64Pwd))
+
+$base64Token = kubectl -n minio-operator get secret console-sa-secret -o jsonpath="{.data.token}"
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($base64Token))
